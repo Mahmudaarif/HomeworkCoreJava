00ec1b61cba3f6056ce51ae4d647867722456b69
Mahmuda Arif <mahmudaarifqa@gmail.com>
2022-11-14 00:09:43 -0500

    homework6

--- src/hw5Q3Constructor/StudentTest.java
+++ src/hw5Q3Constructor/StudentTest.java
@@ -1,12 +1,12 @@
 package hw5Q3Constructor;
 
 public class StudentTest {
+	
 	public static void main(String[] args) {
 		Student st1 = new Student();
 		Student st2 = new Student("Mahmuda Arif", 23765, 'f', true, 3.75f);
 		
 	}
-	
 	
 		
 	}
