1518b920e16f8e8dac30880494eacc34c8e3261a
Mahmuda Arif <mahmudaarifqa@gmail.com>
2022-11-14 19:36:43 -0500

    homework6

--- src/hw6IfElseCondition01/Voter.java
+++ src/hw6IfElseCondition01/Voter.java
@@ -4,16 +4,13 @@ public class Voter {
 	public static void main(String[] args) {
 		int age = 18;
 		if (age == 18) {
-			System.err.println("I am a voter");
+			System.out.println("I am a voter");
 		} else if (age < 18) {
 			System.out.println("I am not a voter");
 		} else if (age > 18) {
 			System.out.println("I am a voter");
-
 		} else {
 			System.out.println("Please add a valid age");
 		}
-
 	}
-
 }
